d26ef3f7cfc204da451ec97ec15f437ef50466d0
Franz Kohnle <franz@kohnlehome.de>
2021-04-17 23:25:43 +0200

    Rename identifiers

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const SYMBOL_EMPTY = '\u00A0';
 const App = {
     data(){
         return {
-            breite: 8,            
+            boardsize: 8,            
             stack: []
         }
     },
     
     computed: {
-        zeilen (){
+        rows (){
             const result = [];
-            for (let i = 0; i < this.breite; i++) {
+            for (let i = 0; i < this.boardsize; i++) {
                 result.push(i);
             }
             return result;
         },
-        spalten (){
+        columns (){
             const result = [];
-            for (let i = 0; i < this.breite; i++) {
+            for (let i = 0; i < this.boardsize; i++) {
                 result.push(i);
             }
             return result;
@@ -27,29 +27,28 @@ const App = {
     },
 
     methods: {
-        klasse(zeile, spalte){
-            if((zeile + spalte) % 2 === 0){
+        displayClass(row, column){
+            if((row + column) % 2 === 0){
                 return 'white';
             }else{
                 return 'black';
             }
         },
-        text(zeile, spalte){
-            if(this.stack[zeile]===spalte)
+        displayText(row, column){
+            if(this.stack[row]===column)
                 return SYMBOL_QUEEN;
             else
                 return SYMBOL_EMPTY;
         },
-        loesegame(zeile){
-            console.log(zeile);
-            this.stack.push(zeile);
-            if(zeile<8){
-                this.const loesegame(zeile+1)
+        findSolutionFromRow(row){
+            this.stack.push(row);
+            if(row<8){
+                this.findSolutionFromRow(row+1)
             }
         }
     },
     mounted(){
-        this.loesegame(0);
+        this.findSolutionFromRow(0);
     }
 };
 Vue.createApp(App).mount('#app');
